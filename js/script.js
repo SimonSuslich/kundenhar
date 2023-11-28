ee75c85a16c2063608bed4e95352878a528fed79
SimonSuslich <simonniklas.suslich@elev.ga.ntig.se>
2023-11-28 12:58:05 +0100

    Copied what I've done before the lesson
    Allt jag har gjort innan lektionen v.48 har jag kopierat in hit och skapat denna hemsida

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Get a NodeList of elements with the class "account-buttons"
+let accountButtons = document.querySelectorAll('.account-buttons');
+
+// Loop through each element in the NodeList and add the event listener
+accountButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    alert("In order to use this feature, you should have an account.")
+    // Add your code here to execute when a button is clicked
+  });
+});
+
+let likeButton = document.querySelectorAll('.like-button');
+
+likeButton.forEach(function(button) {
+  button.addEventListener('click', function () {
+    let heartIcon = button.querySelector('i');
+
+    if (heartIcon.classList.contains("bx-heart")) {
+      heartIcon.classList.remove('bx-heart');
+      heartIcon.classList.add('bxs-heart');
+  } else {
+      heartIcon.classList.remove('bxs-heart');
+      heartIcon.classList.add('bx-heart');
+    }
+  })
+});
