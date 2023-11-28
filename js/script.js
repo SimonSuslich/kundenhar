96a236d9ea1204852714fc5e9dba2af37d81b4be
SimonSuslich <simonniklas.suslich@elev.ga.ntig.se>
2023-11-28 13:48:07 +0100

    skapade pinterest-like layaout på art-for-sale.html

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 let accountButtons = document.querySelectorAll('.account-buttons');
 
 // Loop through each element in the NodeList and add the event listener
-accountButtons.forEach(function(button) {
-  button.addEventListener('click', function() {
+accountButtons.forEach(function (button) {
+  button.addEventListener('click', function () {
     alert("In order to use this feature, you should have an account.")
     // Add your code here to execute when a button is clicked
   });
@@ -11,14 +11,14 @@ accountButtons.forEach(function(button) {
 
 let likeButton = document.querySelectorAll('.like-button');
 
-likeButton.forEach(function(button) {
+likeButton.forEach(function (button) {
   button.addEventListener('click', function () {
     let heartIcon = button.querySelector('i');
 
     if (heartIcon.classList.contains("bx-heart")) {
       heartIcon.classList.remove('bx-heart');
       heartIcon.classList.add('bxs-heart');
-  } else {
+    } else {
       heartIcon.classList.remove('bxs-heart');
       heartIcon.classList.add('bx-heart');
     }
